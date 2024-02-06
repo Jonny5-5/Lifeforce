629f32935d9f026c06fd8505e9c9534fef67980b
Jonathan de Gaston <developerjon5@gmail.com>
2024-02-06 13:11:30 -0700

    Animate based on direction. Up or Down

--- lib/flame_game/components/player.dart
+++ lib/flame_game/components/player.dart
@@ -23,21 +23,10 @@ class Player extends SpriteAnimationGroupComponent<PlayerState>
     required this.addScore,
     required this.resetScore,
     super.position,
-  }) : super(size: Vector2.all(150), anchor: Anchor.center, priority: 1);
+  }) : super(size: Vector2(96, 48), anchor: Anchor.center, priority: 1);
 
   final void Function({int amount}) addScore;
   final VoidCallback resetScore;
-
-  // The current velocity that the player has that comes from being affected by
-  // the gravity. Defined in virtual pixels/s².
-  double _gravityVelocity = 0;
-
-  // The maximum length that the player can jump. Defined in virtual pixels.
-  final double _jumpLength = 600;
-
-  // Whether the player is currently in the air, this can be used to restrict
-  // movement for example.
-  bool get inAir => (position.y + size.y / 2) < world.groundLevel;
 
   // Used to store the last position of the player, so that we later can
   // determine which direction that the player is moving.
@@ -98,27 +87,15 @@ class Player extends SpriteAnimationGroupComponent<PlayerState>
   @override
   void update(double dt) {
     super.update(dt);
-    // When we are in the air the gravity should affect our position and pull
-    // us closer to the ground.
-    // if (inAir) {
-    //   _gravityVelocity += world.gravity * dt;
-    //   position.y += _gravityVelocity;
-    //   if (isFalling) {
-    //     current = PlayerState.falling;
-    //   }
-    // }
 
-    // final belowGround = position.y + size.y / 2 > world.groundLevel;
-    // If the player's new position would overshoot the ground level after
-    // updating its position we need to move the player up to the ground level
-    // again.
-    // if (belowGround) {
-    //   position.y = world.groundLevel - size.y / 2;
-    //   _gravityVelocity = 0;
-    //   current = PlayerState.running;
-    // }
+    if (_lastPosition.y > position.y) {
+      current = PlayerState.flyingUp;
+    } else if (_lastPosition.y < position.y) {
+      current = PlayerState.flyingDown;
+    } else {
+      current = PlayerState.flying;
+    }
 
-    current = PlayerState.flying;
     _lastPosition.setFrom(position);
   }
 
@@ -139,21 +116,6 @@ class Player extends SpriteAnimationGroupComponent<PlayerState>
       game.audioController.playSfx(SfxType.score);
       other.removeFromParent();
       addScore();
-    }
-  }
-
-  /// [towards] should be a normalized vector that points in the direction that
-  /// the player should jump.
-  void jump(Vector2 towards) {
-    current = PlayerState.jumping;
-    // Since `towards` is normalized we need to scale (multiply) that vector by
-    // the length that we want the jump to have.
-    final jumpEffect = JumpEffect(towards..scaleTo(_jumpLength));
-
-    // We only allow jumps when the player isn't already in the air.
-    if (!inAir) {
-      game.audioController.playSfx(SfxType.jump);
-      add(jumpEffect);
     }
   }
 
