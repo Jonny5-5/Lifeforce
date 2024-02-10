8500e9f64df6c9ed7efff722b0f9d5c15b39fabc
Jonathan de Gaston <developerjon5@gmail.com>
2024-02-10 12:27:45 -0700

    Add actual Life Force music because it's amazing

--- lib/audio/songs.dart
+++ lib/audio/songs.dart
@@ -1,7 +1,9 @@
 const List<Song> songs = [
-  Song('bit_forrest.mp3', 'Bit Forrest', artist: 'bertz'),
-  Song('free_run.mp3', 'Free Run', artist: 'TAD'),
-  Song('tropical_fantasy.mp3', 'Tropical Fantasy', artist: 'Spring Spring'),
+  // Song('bit_forrest.mp3', 'Bit Forrest', artist: 'bertz'),
+  // Song('free_run.mp3', 'Free Run', artist: 'TAD'),
+  // Song('tropical_fantasy.mp3', 'Tropical Fantasy', artist: 'Spring Spring'),
+  Song('life_force3.mp3', 'Life Force - Stage 3', artist: 'Konami'),
+  Song('life_force5.mp3', 'Life Force - Stage 5', artist: 'Konami'),
 ];
 
 class Song {
